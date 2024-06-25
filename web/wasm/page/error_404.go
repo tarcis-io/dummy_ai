b9b3390ddd9599267fab18001ce68da036e9c5e4
Tarcisio Oliveira <t@rcis.io>
2024-06-25 17:30:08 -0300

    update

--- web/wasm/page/error_404.go
+++ web/wasm/page/error_404.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	h2 := js.Global().Get("document").Call("createElement", "h2")
-	h2.Set("innerHTML", "Index")
+	h2.Set("innerHTML", "Error 404!")
 
 	body := js.Global().Get("document").Get("body")
 	body.Call("appendChild", h2)
