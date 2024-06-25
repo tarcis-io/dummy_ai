fa6751e6b8bef32e53738c06da935ba80528d2ac
Tarcisio Oliveira <t@rcis.io>
2024-06-25 16:59:14 -0300

    update

--- web/server/server.go
+++ web/server/server.go
@@ -15,6 +15,19 @@ var (
 
 func Start() {
 
+	serveFile("/manifest.json" /*        */, "./web/static/config/manifest.json")
+	serveFile("/robots.txt" /*           */, "./web/static/config/robots.txt")
+	serveFile("/sitemap.xml" /*          */, "./web/static/config/sitemap.xml")
+	serveFile("/apple_touch_icon.png" /* */, "./web/static/img/favicon/apple_touch_icon.png")
+	serveFile("/favicon.ico" /*          */, "./web/static/img/favicon/favicon.ico")
+	serveFile("/favicon.svg" /*          */, "./web/static/img/favicon/favicon.svg")
+	serveFile("/favicon_192x192.png" /*  */, "./web/static/img/favicon/favicon_192x192.png")
+	serveFile("/favicon_512x512.png" /*  */, "./web/static/img/favicon/favicon_512x512.png")
+	serveFile("/logo.svg" /*             */, "./web/static/img/logo/logo.svg")
+	serveFile("/logo_white.svg" /*       */, "./web/static/img/logo/logo_white.svg")
+	serveFile("/wasm_exec.js" /*         */, "./web/static/lib/wasm/wasm_exec.js")
+	serveFile("/wasm_start.js" /*        */, "./web/static/lib/wasm/wasm_start.js")
+
 	if err := http.ListenAndServe(env.ServerAddress, nil); err != nil {
 
 		panic(err)
