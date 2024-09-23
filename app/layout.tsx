7b8e98e32d3be4c02041ac73958d247c77d3d55a
Tarcisio Oliveira <t@rcis.io>
2024-09-23 15:29:39 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import {
+	Page,
+	PageSection
+} from '@patternfly/react-core';
+
 export default function Layout({ children }) {
 
 	return (
-		<html>
-			<body>{ children }</body>
+		<html lang = { 'en' }>
+			<body>
+				<Page>
+					<PageSection>{ children }</PageSection>
+				</Page>
+			</body>
 		</html>
 	);
 };
