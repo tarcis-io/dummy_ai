487449ec40f2fd13667671a8a0880d1c5a1a51f4
Tarcisio Oliveira <t@rcis.io>
2024-09-23 22:27:04 -0300

    update

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import {
+	Page,
+	PageSection
+} from '@patternfly/react-core';
+
+export default function Layout({ children }) {
+
+	return (
+		<html lang = { 'en' }>
+			<body>
+				<Page>
+					<PageSection>{ children }</PageSection>
+				</Page>
+			</body>
+		</html>
+	);
+};
