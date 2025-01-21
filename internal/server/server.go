8b5ff604c26a5b59133c243a3c309958624df52d
Tarcisio Oliveira <t@rcis.io>
2025-01-20 22:58:53 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,4 +1,18 @@
 package server
 
+import (
+	"net/http"
+)
+
 func Start() {
+
+	listenAndServe()
+}
+
+func listenAndServe() {
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+		panic(err)
+	}
 }
