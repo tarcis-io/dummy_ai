62d17cbf921ee20c6baad79633f1f5a5ea42d33f
Tarcisio Oliveira <t@rcis.io>
2025-01-27 12:34:52 -0300

    update

--- internal/wasm/util/message.go
+++ internal/wasm/util/message.go
@@ -6,8 +6,21 @@ const (
 )
 
 var (
-	allMessages = map[string]map[int]string{}
-	messages    = allMessages[language]
+	allMessages = map[string]map[int]string{
+		English: {
+			app:            "DummyAI",
+			appDescription: "Artificial intelligence for dummies",
+		},
+		Spanish: {
+			app:            "DummyAI",
+			appDescription: "Artificial intelligence for dummies",
+		},
+		Portuguese: {
+			app:            "DummyAI",
+			appDescription: "Artificial intelligence for dummies",
+		},
+	}
+	messages = allMessages[language]
 )
 
 func App() string {
