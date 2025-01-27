8b063ed5b48b0d452b2073a7e4c56637e2561e4c
Tarcisio Oliveira <t@rcis.io>
2025-01-27 12:29:15 -0300

    update

--- internal/wasm/util/message.go
+++ internal/wasm/util/message.go
@@ -4,3 +4,18 @@ const (
 	app = iota
 	appDescription
 )
+
+var (
+	allMessages = map[string]map[int]string{}
+	messages    = allMessages[language]
+)
+
+func App() string {
+
+	return messages[app]
+}
+
+func AppDescription() string {
+
+	return messages[appDescription]
+}
