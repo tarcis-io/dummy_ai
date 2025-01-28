30a0d6121b2cd7ebd7dadf0d60802f817d04cd94
Tarcisio Oliveira <t@rcis.io>
2025-01-28 06:37:03 -0300

    update

--- internal/wasm/component/app.go
+++ internal/wasm/component/app.go
@@ -6,8 +6,12 @@ import (
 
 func CreateApp(pageContent js.Value) js.Value {
 
+	masthead := js.Global().Get("document").Call("createElement", "header")
+	masthead.Set("className", "pf-v6-c-masthead")
+
 	page := js.Global().Get("document").Call("createElement", "div")
 	page.Set("className", "pf-v6-c-page")
+	page.Call("appendChild", masthead)
 
 	return page
 }
