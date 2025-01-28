98b3dbf3f398d714f2d0fda0fafa9c4142b6e220
Tarcisio Oliveira <t@rcis.io>
2025-01-28 17:56:13 -0300

    update

--- internal/wasm/component/app.go
+++ internal/wasm/component/app.go
@@ -6,15 +6,12 @@ import (
 
 func CreateApp(pageContent js.Value) js.Value {
 
-	mastheadToggle := js.Global().Get("document").Call("createElement", "span")
-	mastheadToggle.Set("className", "pf-v6-c-masthead__toggle")
-
 	mastheadMain := js.Global().Get("document").Call("createElement", "div")
 	mastheadMain.Set("classNAme", "pf-v6-c-masthead__main")
-	mastheadMain.Call("appendChild", mastheadToggle)
 
 	masthead := js.Global().Get("document").Call("createElement", "header")
 	masthead.Set("className", "pf-v6-c-masthead")
+	masthead.Call("appendChild", mastheadMain)
 
 	page := js.Global().Get("document").Call("createElement", "div")
 	page.Set("className", "pf-v6-c-page")
