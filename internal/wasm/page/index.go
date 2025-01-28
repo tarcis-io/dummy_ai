d37e7315e7734e3810af6dbf827eeb5001ac9b23
Tarcisio Oliveira <t@rcis.io>
2025-01-28 00:34:02 -0300

    update

--- internal/wasm/page/index.go
+++ internal/wasm/page/index.go
@@ -4,13 +4,19 @@ import (
 	"syscall/js"
 )
 
+import (
+	"dummy_ai/internal/wasm/component"
+)
+
 func main() {
 
 	h2 := js.Global().Get("document").Call("createElement", "h2")
 	h2.Set("innerHTML", "index.wasm")
 
+	app := component.CreateApp(h2)
+
 	body := js.Global().Get("document").Get("body")
-	body.Call("appendChild", h2)
+	body.Call("appendChild", app)
 
 	select {}
 }
