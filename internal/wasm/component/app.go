7fa9f31684c5eae8a1ca5ddc4c7b9503505307e4
Tarcisio Oliveira <t@rcis.io>
2025-01-29 20:18:32 -0300

    update

--- internal/wasm/component/app.go
+++ internal/wasm/component/app.go
@@ -4,10 +4,32 @@ import (
 	"syscall/js"
 )
 
+import (
+	"dummy_ai/internal/wasm/util"
+)
+
 func CreateApp(pageContent js.Value) js.Value {
+
+	navigationIcon := js.Global().Get("document").Call("createElement", "i")
+	navigationIcon.Set("className", "fas fa-bars")
+	navigationIcon.Set("ariaHidden", true)
+
+	navigationButtonIcon := js.Global().Get("document").Call("createElement", "span")
+	navigationButtonIcon.Set("className", "pf-v6-c-button__icon")
+	navigationButtonIcon.Call("appendChild", navigationIcon)
+
+	navigationButton := js.Global().Get("document").Call("createElement", "button")
+	navigationButton.Set("className", "pf-v6-button pf-m-plain")
+	navigationButton.Set("ariaLabel", util.AppButtonNavigationAriaLabel())
+	navigationButton.Call("appendChild", navigationButtonIcon)
+
+	mastheadToggle := js.Global().Get("document").Call("createElement", "span")
+	mastheadToggle.Set("className", "pf-v6-c-masthead__toggle")
+	mastheadToggle.Call("appendChild", navigationButton)
 
 	mastheadMain := js.Global().Get("document").Call("createElement", "div")
 	mastheadMain.Set("className", "pf-v6-c-masthead__main")
+	mastheadMain.Call("appendChild", mastheadToggle)
 
 	masthead := js.Global().Get("document").Call("createElement", "header")
 	masthead.Set("className", "pf-v6-c-masthead")
