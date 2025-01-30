64aa0efc7032e7c929205a031dbbfa8654c97290
Tarcisio Oliveira <t@rcis.io>
2025-01-29 23:30:38 -0300

    update

--- internal/wasm/component/masthead.go
+++ internal/wasm/component/masthead.go
@@ -10,6 +10,14 @@ func CreateMasthead() js.Value {
 	navigationIcon.Set("className", "fas fa-bars")
 	navigationIcon.Set("ariaHidden", true)
 
+	navigationButtonIcon := js.Global().Get("document").Call("createElement", "span")
+	navigationButtonIcon.Set("className", "pf-v6-c-button__icon")
+	navigationButtonIcon.Call("appendChild", navigationIcon)
+
+	navigationButton := js.Global().Get("document").Call("createElement", "button")
+	navigationButton.Set("className", "pf-v6-c-button pf-m-plain")
+	navigationButton.Call("appendChild", navigationButtonIcon)
+
 	mastheadToggle := js.Global().Get("document").Call("createElement", "span")
 	mastheadToggle.Set("className", "pf-v6-c-masthead__toggle")
 
