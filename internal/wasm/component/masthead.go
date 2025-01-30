6532fba5529777415ed53e86a69aba8bef5832e0
Tarcisio Oliveira <t@rcis.io>
2025-01-29 23:10:08 -0300

    update

--- internal/wasm/component/masthead.go
+++ internal/wasm/component/masthead.go
@@ -1,1 +1,13 @@
 package component
+
+import (
+	"syscall/js"
+)
+
+func CreateMasthead() js.Value {
+
+	masthead := js.Global().Get("document").Call("createElement", "header")
+	masthead.Set("className", "pf-v6-c-masthead")
+
+	return masthead
+}
