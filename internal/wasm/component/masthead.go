1cfd18c770cb8eb3a7d4ecb172bcf9019695b460
Tarcisio Oliveira <t@rcis.io>
2025-01-29 23:53:04 -0300

    update

--- internal/wasm/component/masthead.go
+++ internal/wasm/component/masthead.go
@@ -28,9 +28,18 @@ func CreateMasthead() js.Value {
 	mastheadToggle.Set("className", "pf-v6-c-masthead__toggle")
 	mastheadToggle.Call("appendChild", navigationButton)
 
+	mastheadLogo := js.Global().Get("document").Call("createElement", "a")
+	mastheadLogo.Set("className", "pf-v6-c-masthead__logo")
+	mastheadLogo.Set("href", "/")
+
+	mastheadBrand := js.Global().Get("document").Call("createElement", "div")
+	mastheadBrand.Set("className", "pf-v6-c-masthead__brand")
+	mastheadBrand.Call("appendChild", mastheadLogo)
+
 	mastheadMain := js.Global().Get("document").Call("createElement", "div")
 	mastheadMain.Set("className", "pf-v6-c-masthead__main")
 	mastheadMain.Call("appendChild", mastheadToggle)
+	mastheadMain.Call("appendChild", mastheadBrand)
 
 	masthead := js.Global().Get("document").Call("createElement", "header")
 	masthead.Set("className", "pf-v6-c-masthead")
