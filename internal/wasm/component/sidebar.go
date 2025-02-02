8c5115fee0fd74c715eeb925c9555cc887146e46
Tarcisio Oliveira <t@rcis.io>
2025-02-02 18:04:14 -0300

    update

--- internal/wasm/component/sidebar.go
+++ internal/wasm/component/sidebar.go
@@ -52,6 +52,7 @@ func createSidebarNavigationItem(text string, href string) js.Value {
 
 	if pathname := js.Global().Get("location").Get("pathname").String(); pathname == href {
 
+		navigationLink.Set("ariaCurrent", "page")
 		navigationLink.Get("classList").Call("add", "pf-m-current")
 	}
 
