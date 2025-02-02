ae0455f1b4f3fe2249bbdb146c741ab483741355
Tarcisio Oliveira <t@rcis.io>
2025-02-02 01:04:28 -0300

    update

--- internal/wasm/component/sidebar.go
+++ internal/wasm/component/sidebar.go
@@ -1,1 +1,13 @@
 package component
+
+import (
+	"syscall/js"
+)
+
+func CreateSidebar() js.Value {
+
+	sidebar := js.Global().Get("document").Call("createElement", "div")
+	sidebar.Set("className", "pf-v6-c-page__sidebar")
+
+	return sidebar
+}
