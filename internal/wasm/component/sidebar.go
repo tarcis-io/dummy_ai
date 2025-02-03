5a4401483feeb50b1d77914eb3f1e77192b24fb3
Tarcisio Oliveira <t@rcis.io>
2025-02-03 13:34:59 -0300

    update

--- internal/wasm/component/sidebar.go
+++ internal/wasm/component/sidebar.go
@@ -53,6 +53,7 @@ func CreateSidebar() js.Value {
 
 	sidebar := js.Global().Get("document").Call("createElement", "div")
 	sidebar.Set("className", "pf-v6-c-page__sidebar")
+	sidebar.Set("id", "sidebar")
 	sidebar.Call("appendChild", sidebarBody)
 
 	return sidebar
