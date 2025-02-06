8261e5f7e63644e39cc0bfcedc6aeb4f1df1094a
Tarcisio Oliveira <t@rcis.io>
2025-02-06 18:54:23 -0300

    update

--- internal/wasm/page/error_404.go
+++ internal/wasm/page/error_404.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"syscall/js"
+)
+
+import (
+	"dummy_ai/internal/wasm/component"
+)
+
 func main() {
+
+	pageContent := js.Global().Get("document").Call("createElement", "div")
+
+	app := component.CreateApp(pageContent)
+
+	body := js.Global().Get("document").Get("body")
+	body.Call("appendChild", app)
 
 	select {}
 }
