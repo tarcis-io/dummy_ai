f3c306cb69269eba76313fd15bad2fd32a2a8225
Tarcisio Oliveira <t@rcis.io>
2025-02-06 00:18:37 -0300

    update

--- internal/wasm/component/app.go
+++ internal/wasm/component/app.go
@@ -6,9 +6,14 @@ import (
 
 func CreateApp(pageContent js.Value) js.Value {
 
+	pageMain := js.Global().Get("document").Call("createElement", "main")
+	pageMain.Set("className", "pf-v6-c-page__main")
+	pageMain.Set("tabIndex", -1)
+
 	pageMainContainer := js.Global().Get("document").Call("createElement", "div")
 	pageMainContainer.Set("className", "pf-v6-c-page__main-container pf-m-fill")
 	pageMainContainer.Set("tabIndex", -1)
+	pageMainContainer.Call("appendChild", pageMain)
 
 	page := js.Global().Get("document").Call("createElement", "div")
 	page.Set("className", "pf-v6-c-page")
