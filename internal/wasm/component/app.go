1a3bfb40a69efc2eb12ce1fa3438968d34af6079
Tarcisio Oliveira <t@rcis.io>
2025-02-06 00:06:35 -0300

    update

--- internal/wasm/component/app.go
+++ internal/wasm/component/app.go
@@ -12,6 +12,7 @@ func CreateApp(pageContent js.Value) js.Value {
 
 	page := js.Global().Get("document").Call("createElement", "div")
 	page.Set("className", "pf-v6-c-page")
+	page.Get("style").Call("setProperty", "--pf-v6-page__sidebar--xl--TranslateX", "-100%")
 	page.Call("appendChild", CreateMasthead())
 	page.Call("appendChild", CreateSidebar())
 	page.Call("appendChild", pageMainContainer)
