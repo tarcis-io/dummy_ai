e7d85290c5eb168365270ef72a917e004f2c2ac9
Tarcisio Oliveira <t@rcis.io>
2025-02-06 20:07:46 -0300

    update

--- internal/wasm/page/error_404.go
+++ internal/wasm/page/error_404.go
@@ -6,7 +6,15 @@ import (
 
 import (
 	"dummy_ai/internal/wasm/component"
+	"dummy_ai/internal/wasm/util"
 )
+
+func init() {
+
+	html := js.Global().Get("document").Get("documentElement")
+	html.Set("className", "pf-v6-theme-dark")
+	html.Set("lang", util.Language())
+}
 
 func main() {
 
