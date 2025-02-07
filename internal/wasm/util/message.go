088e21baeae5e3ebae08e83df6d3855db59e4726
Tarcisio Oliveira <t@rcis.io>
2025-02-06 23:46:17 -0300

    update

--- internal/wasm/util/message.go
+++ internal/wasm/util/message.go
@@ -3,21 +3,29 @@ package util
 const (
 	app = iota
 	appDescription
+	navigationItemAbout
+	navigationItemHome
 )
 
 var (
 	allMessages = map[string]map[int]string{
 		English: {
-			app:            "DummyAI",
-			appDescription: "Artificial intelligence for dummies",
+			app:                 "DummyAI",
+			appDescription:      "Artificial intelligence for dummies",
+			navigationItemAbout: "About",
+			navigationItemHome:  "Home",
 		},
 		Spanish: {
-			app:            "DummyAI",
-			appDescription: "Artificial intelligence for dummies",
+			app:                 "DummyAI",
+			appDescription:      "Artificial intelligence for dummies",
+			navigationItemAbout: "About",
+			navigationItemHome:  "Home",
 		},
 		Portuguese: {
-			app:            "DummyAI",
-			appDescription: "Artificial intelligence for dummies",
+			app:                 "DummyAI",
+			appDescription:      "Artificial intelligence for dummies",
+			navigationItemAbout: "Sobre",
+			navigationItemHome:  "Início",
 		},
 	}
 	messages = allMessages[language]
