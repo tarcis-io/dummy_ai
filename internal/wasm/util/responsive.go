6df494aa189a639abec723d0ced507e4f10edae1
Tarcisio Oliveira <t@rcis.io>
2025-02-07 17:25:22 -0300

    update

--- internal/wasm/util/responsive.go
+++ internal/wasm/util/responsive.go
@@ -6,7 +6,7 @@ import (
 
 func IsDesktop() bool {
 
-	breakpoint := js.Global().Call("getComputedStyle", js.Global().Get("document").Get("documentElement")).Call("getPropertyValue", "--pf-t--global--breakpoint--md").String()
+	breakpoint := js.Global().Call("getComputedStyle", js.Global().Get("document").Get("documentElement")).Call("getPropertyValue", "--pf-t--global--breakpoint--xl").String()
 	mediaQuery := "screen and (min-width: " + breakpoint + ")"
 
 	return js.Global().Call("matchMedia", mediaQuery).Get("matches").Bool()
