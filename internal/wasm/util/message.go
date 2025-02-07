0e10433493206f3fe5c4762b917bfe73375614aa
Tarcisio Oliveira <t@rcis.io>
2025-02-06 23:39:07 -0300

    update

--- internal/wasm/util/message.go
+++ internal/wasm/util/message.go
@@ -3,34 +3,13 @@ package util
 const (
 	app = iota
 	appDescription
-	mastheadNavigationButtonAriaLabel
-	sidebarAboutNavigationItem
-	sidebarHomeNavigationItem
 )
 
 var (
 	allMessages = map[string]map[int]string{
-		English: {
-			app:                               "DummyAI",
-			appDescription:                    "Artificial intelligence for dummies",
-			mastheadNavigationButtonAriaLabel: "Navigation",
-			sidebarAboutNavigationItem:        "About",
-			sidebarHomeNavigationItem:         "Home",
-		},
-		Spanish: {
-			app:                               "DummyAI",
-			appDescription:                    "Artificial intelligence for dummies",
-			mastheadNavigationButtonAriaLabel: "Navegación",
-			sidebarAboutNavigationItem:        "Sobre",
-			sidebarHomeNavigationItem:         "Inicio",
-		},
-		Portuguese: {
-			app:                               "DummyAI",
-			appDescription:                    "Artificial intelligence for dummies",
-			mastheadNavigationButtonAriaLabel: "Navegação",
-			sidebarAboutNavigationItem:        "Sobre",
-			sidebarHomeNavigationItem:         "Início",
-		},
+		English:    {},
+		Spanish:    {},
+		Portuguese: {},
 	}
 	messages = allMessages[language]
 )
@@ -43,19 +22,4 @@ func App() string {
 func AppDescription() string {
 
 	return messages[appDescription]
-}
-
-func MastheadNavigationButtonAriaLabel() string {
-
-	return messages[mastheadNavigationButtonAriaLabel]
-}
-
-func SidebarAboutNavigationItem() string {
-
-	return messages[sidebarAboutNavigationItem]
-}
-
-func SidebarHomeNavigationItem() string {
-
-	return messages[sidebarHomeNavigationItem]
 }
