793c21faa4cdfe343b26110fb2ee9af4d7578b1c
Tarcisio Oliveira <t@rcis.io>
2025-02-11 12:56:49 -0300

    update

--- internal/wasm/page/home.go
+++ internal/wasm/page/home.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"syscall/js"
+)
+
+import (
+	"dummy_ai/internal/wasm/util"
+)
+
 func init() {
+
+	html := js.Global().Get("document").Get("documentElement")
+	html.Set("className", "pf-v6-theme-dark")
+	html.Set("lang", util.Language())
 }
 
 func main() {
