124eed4dad8b81a6bfc9772baf5770286d4d3d3d
Tarcisio Oliveira <t@rcis.io>
2025-02-12 20:40:52 -0300

    update

--- internal/wasm/page/home.go
+++ internal/wasm/page/home.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"syscall/js"
-)
-
-import (
 	"dummy_ai/internal/wasm/component"
-	"dummy_ai/internal/wasm/util"
 )
-
-func init() {
-
-	html := js.Global().Get("document").Get("documentElement")
-	html.Set("className", "pf-v6-theme-dark")
-	html.Set("lang", util.Language())
-}
 
 func main() {
 
-	body := js.Global().Get("document").Get("body")
-	body.Call("appendChild", component.CreateApp(component.CreateCamera()))
-
+	component.CreateApp(component.CreateCamera())
 	select {}
 }
