5513bb6334b0a12c9e72cf86377bfac2f283768c
Tarcisio Oliveira <t@rcis.io>
2025-02-14 17:13:00 -0300

    update

--- internal/wasm/page/about.go
+++ internal/wasm/page/about.go
@@ -10,8 +10,12 @@ import (
 
 func main() {
 
+	cardTitle := js.Global().Get("document").Call("createElement", "title")
+	cardTitle.Set("className", "pf-v6-c-card__title")
+
 	card := js.Global().Get("document").Call("createElement", "div")
 	card.Set("className", "pf-v6-c-card")
+	card.Call("appendChild", cardTitle)
 
 	component.CreateApp(card)
 	select {}
