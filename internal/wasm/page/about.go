4d13ca5653aacf5e8b7ea898c2ecc173a6e0244e
Tarcisio Oliveira <t@rcis.io>
2025-02-14 17:17:08 -0300

    update

--- internal/wasm/page/about.go
+++ internal/wasm/page/about.go
@@ -6,12 +6,18 @@ import (
 
 import (
 	"dummy_ai/internal/wasm/component"
+	"dummy_ai/internal/wasm/util"
 )
 
 func main() {
 
+	cardTitleText := js.Global().Get("document").Call("createElement", "h2")
+	cardTitleText.Set("className", "pf-v6-c-card__title-text")
+	cardTitleText.Set("innerText", util.App())
+
 	cardTitle := js.Global().Get("document").Call("createElement", "title")
 	cardTitle.Set("className", "pf-v6-c-card__title")
+	cardTitle.Call("appendChild", cardTitleText)
 
 	card := js.Global().Get("document").Call("createElement", "div")
 	card.Set("className", "pf-v6-c-card")
