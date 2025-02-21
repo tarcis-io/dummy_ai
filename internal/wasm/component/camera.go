36f0e98b5d6d0cf15d36d35589eb2a4ce94beb59
Tarcisio Oliveira <t@rcis.io>
2025-02-21 01:17:21 -0300

    update

--- internal/wasm/component/camera.go
+++ internal/wasm/component/camera.go
@@ -11,8 +11,12 @@ func CreateCamera() js.Value {
 
 func CreateCameraError(title string, text string) js.Value {
 
+	cardTitle := js.Global().Get("document").Call("createElement", "div")
+	cardTitle.Set("className", "pf-v6-c-card__title")
+
 	card := js.Global().Get("document").Call("createElement", "div")
 	card.Set("className", "pf-v6-c-card")
+	card.Call("appendChild", cardTitle)
 
 	return card
 }
