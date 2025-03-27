6a841a7a7b76c3f9b3099050043ae321a9f6bf0e
Tarcisio Oliveira <t@rcis.io>
2025-03-27 19:10:43 -0300

    update

--- static/lib/wasm/wasm_start.js
+++ static/lib/wasm/wasm_start.js
@@ -3,5 +3,5 @@
 const wasmStart = (wasmRoute) => {
 
 	const go = new Go();
-	WebAssembly.instantiateStreaming(fetch(wasmRoute), go.importObject);
+	WebAssembly.instantiateStreaming(fetch(wasmRoute), go.importObject).then(webAssemblyInstantiatedSource => go.run(webAssemblyInstantiatedSource.instance));
 };
