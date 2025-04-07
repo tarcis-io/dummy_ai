23b6a924192db67c769163b1f26ae73e26361ca0
Tarcisio Oliveira <t@rcis.io>
2025-04-07 17:37:14 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,15 +2,28 @@ package server
 
 import (
 	"net/http"
+	"text/template"
 )
 
 import (
 	"dummy_ai/internal/env"
 )
 
+var (
+	serverTemplate = template.Must(template.ParseFiles("./static/server/template.html"))
+)
+
 func Start() {
 
 	listenAndServe()
+}
+
+func executeServerTemplate(responseWriter http.ResponseWriter, wasmRoute string) {
+
+	if err := serverTemplate.Execute(responseWriter, wasmRoute); err != nil {
+
+		panic(err)
+	}
 }
 
 func listenAndServe() {
