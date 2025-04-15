73ccbcb30275bc022bf671aa215aa806c253aa51
Tarcisio Oliveira <t@rcis.io>
2025-04-15 02:27:58 -0300

    update

--- internal/wasm/dom/window.go
+++ internal/wasm/dom/window.go
@@ -6,6 +6,15 @@ type (
 	}
 )
 
+func (window Window) Document() Document {
+
+	return Document{
+		DOM: DOM{
+			jsObject: window.jsObject.Get("document"),
+		},
+	}
+}
+
 func GetWindow() Window {
 
 	return Window{
