6ff1c4e0a8d34ac16e65e192a90c2b0dd88325ab
Tarcisio Oliveira <t@rcis.io>
2025-04-16 19:11:02 -0300

    update

--- internal/wasm/dom/dom.go
+++ internal/wasm/dom/dom.go
@@ -10,6 +10,13 @@ type (
 	}
 )
 
+func (dom DOM) Call(method string, args ...any) DOM {
+
+	return DOM{
+		jsObject: dom.jsObject.Call(method, args...),
+	}
+}
+
 func GetGlobal() js.Value {
 
 	return js.Global()
