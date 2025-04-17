55e135def11360b8e8010168f0f50c7e9fdcd9ad
Tarcisio Oliveira <t@rcis.io>
2025-04-17 18:20:53 -0300

    update

--- internal/wasm/dom/dom.go
+++ internal/wasm/dom/dom.go
@@ -10,6 +10,18 @@ type (
 	}
 )
 
+func (dom DOM) Get(property string) DOM {
+
+	return DOM{
+		jsObject: dom.jsObject.Get(property),
+	}
+}
+
+func (dom DOM) Set(property string, value any) {
+
+	dom.jsObject.Set(property, value)
+}
+
 func (dom DOM) Call(method string, args ...any) DOM {
 
 	return DOM{
