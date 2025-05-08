b0259488eb3f382c0e9040f350e82a72d479dd45
Tarcisio Oliveira <t@rcis.io>
2025-05-08 19:20:27 -0300

    update

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,6 +4,15 @@ import (
 	"os"
 )
 
+var (
+	serverAddress = env("SERVER_ADDRESS", ":8080")
+)
+
+func ServerAddress() string {
+
+	return serverAddress
+}
+
 func env(key string, defaultValue string) string {
 
 	if value, found := os.LookupEnv(key); found {
