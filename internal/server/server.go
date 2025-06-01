e1f778c1ce6b197d176bc07c9077e8a9821faa01
Tarcisio Oliveira <t@rcis.io>
2025-06-01 00:45:15 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -18,6 +18,20 @@ func Start() {
 	listenAndServe()
 }
 
+func handlePage(route string, wasmRoute string) {
+
+	http.HandleFunc(route, func(responseWriter http.ResponseWriter, request *http.Request) {
+
+		if request.URL.Path != route {
+
+			handlePageError404(responseWriter)
+			return
+		}
+
+		executeServerTemplate(responseWriter, wasmRoute)
+	})
+}
+
 func handlePageError404(responseWriter http.ResponseWriter) {
 
 	responseWriter.WriteHeader(http.StatusNotFound)
