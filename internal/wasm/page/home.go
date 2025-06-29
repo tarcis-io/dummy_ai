878a371737edea2ddbf3de57e1142a92d237f6a9
Tarcisio Oliveira <t@rcis.io>
2025-06-29 16:42:26 -0300

    update

---
+++ internal/wasm/page/home.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"dummy_ai/internal/wasm/dom"
+)
+
+func main() {
+
+	h2 := dom.GetDocument().CreateElement("h2")
+	h2.Set("innerText", "home.go")
+
+	body := dom.GetDocument().Get("body")
+	body.Call("appendChild", h2)
+}
