09cc959a777fb223a5604173ee822fc60a3064a5
Tarcisio Oliveira <t@rcis.io>
2025-06-29 01:17:14 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,6 +26,12 @@ func serveFile(route string, file string) {
 	})
 }
 
+func servePageError404(responseWriter http.ResponseWriter) {
+
+	responseWriter.WriteHeader(http.StatusNotFound)
+	executeServerTemplate(responseWriter, "/error_404.wasm")
+}
+
 func executeServerTemplate(responseWriter http.ResponseWriter, wasmRoute string) {
 
 	if err := serverTemplate.Execute(responseWriter, wasmRoute); err != nil {
