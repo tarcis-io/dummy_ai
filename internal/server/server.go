f2ffbf47699e2580308f7c5ea280475a3620167a
Tarcisio Oliveira <t@rcis.io>
2025-06-29 01:56:35 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,7 +11,17 @@ import (
 
 var (
 	staticFiles = map[string]string{
-		"/manifest.json": "./static/config/manifest.json",
+		"/manifest.json":        "./static/config/manifest.json",
+		"/robots.txt":           "./static/config/robots.txt",
+		"/sitemap.xml":          "./static/config/sitemap.xml",
+		"/apple_touch_icon.png": "./static/img/favicon/apple_touch_icon.png",
+		"/favicon.ico":          "./static/img/favicon/favicon.ico",
+		"/favicon.svg":          "./static/img/favicon/favicon.svg",
+		"/favicon_192.png":      "./static/img/favicon/favicon_192.png",
+		"/favicon_512.png":      "./static/img/favicon/favicon_512.png",
+		"/favicon_512_maskable": "./static/img/favicon/favicon_512_maskable.png",
+		"/logo.svg":             "./static/img/logo/logo.svg",
+		"/logo_white.svg":       "./static/img/logo/logo_white.svg",
 	}
 )
 
