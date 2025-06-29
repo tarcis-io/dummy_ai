1beb657e4df1ce51d72136d284795673b0b0aa5c
Tarcisio Oliveira <t@rcis.io>
2025-06-29 01:12:55 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,6 +26,14 @@ func serveFile(route string, file string) {
 	})
 }
 
+func executeServerTemplate(responseWriter http.ResponseWriter, wasmRoute string) {
+
+	if err := serverTemplate.Execute(responseWriter, wasmRoute); err != nil {
+
+		panic(err)
+	}
+}
+
 func listenAndServe() {
 
 	if err := http.ListenAndServe(env.ServerAddress(), nil); err != nil {
