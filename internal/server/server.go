143479ba6391941ce0ed8eca5989fa25c0b2cf61
Tarcisio Oliveira <t@rcis.io>
2025-07-03 19:19:19 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,8 +6,21 @@ import (
 	"dummy_ai/internal/env"
 )
 
+var (
+	staticFiles = map[string]string{}
+)
+
 func ListenAndServe() {
+	for route, file := range staticFiles {
+		serveFile(route, file)
+	}
 	listenAndServe()
+}
+
+func serveFile(route string, file string) {
+	http.HandleFunc(route, func(responseWriter http.ResponseWriter, request *http.Request) {
+		http.ServeFile(responseWriter, request, file)
+	})
 }
 
 func listenAndServe() {
