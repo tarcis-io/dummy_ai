ebd060caa265729fa6cdc44599338f081250139b
Tarcisio Oliveira <t@rcis.io>
2025-07-12 02:41:18 +0000

    update

--- internal/wasm/dom/navigator.go
+++ internal/wasm/dom/navigator.go
@@ -6,6 +6,13 @@ type (
 	}
 )
 
+func (navigator Navigator) Language() (string, bool) {
+	if language := navigator.Get("language"); language.Truthy() {
+		return language.String(), true
+	}
+	return "", false
+}
+
 func GetNavigator() Navigator {
 	return GetWindow().Navigator()
 }
