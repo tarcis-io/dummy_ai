0bbc8a832fb41de70e4d1f59c678570756d5e84a
Tarcisio Oliveira <t@rcis.io>
2025-07-12 01:41:59 +0000

    update

--- internal/wasm/dom/document.go
+++ internal/wasm/dom/document.go
@@ -6,6 +6,10 @@ type (
 	}
 )
 
+func (document Document) CreateElement(element string) DOM {
+	return document.Call("createElement", element)
+}
+
 func GetDocument() Document {
 	return GetWindow().Document()
 }
