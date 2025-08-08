25263031a24990120f5d5169b2597b10100e9091
Tarcisio Oliveira <t@rcis.io>
2025-08-08 23:12:30 +0000

    update

--- env/env.go
+++ env/env.go
@@ -1,58 +1,49 @@
-// Package env provides access to environment variables.
 package env
 
 import (
+	"fmt"
+	"net"
 	"os"
 	"sync"
 )
 
 type (
-	// Config represents the configuration settings for the application.
 	Config struct {
-		languageCode  string
 		serverAddress string
-		themeCode     string
 	}
 )
 
-// LanguageCode returns the language code for the application.
-func (c *Config) LanguageCode() string {
-	return c.languageCode
-}
-
-// ServerAddress returns the server address for the application.
 func (c *Config) ServerAddress() string {
 	return c.serverAddress
 }
 
-// ThemeCode returns the theme code for the application.
-func (c *Config) ThemeCode() string {
-	return c.themeCode
-}
-
 var (
-	// currentConfig holds the current configuration settings for the application.
-	currentConfig *Config
+	serverAddressKey     = "SERVER_ADDRESS"
+	defaultServerAddress = ":8080"
 
-	// currentConfigOnce is used to ensure that the current configuration settings
-	// are only initialized once.
-	currentConfigOnce sync.Once
+	appConfig *Config
+
+	appConfigOnce sync.Once
 )
 
-// CurrentConfig returns the current configuration settings for the application.
-func CurrentConfig() *Config {
-	currentConfigOnce.Do(func() {
-		currentConfig = &Config{
-			languageCode:  lookupEnv("LANGUAGE_CODE", "en"),
-			serverAddress: lookupEnv("SERVER_ADDRESS", ":8080"),
-			themeCode:     lookupEnv("THEME_CODE", "dark"),
+func AppConfig() *Config {
+	appConfigOnce.Do(func() {
+		appConfig = &Config{
+			serverAddress: lookupServerAddressEnv(),
 		}
 	})
-	return currentConfig
+	return appConfig
 }
 
-// lookupEnv retrieves the value of the environment variable named by the key
-// or the default value if the variable is not present.
+func lookupServerAddressEnv() string {
+	v := lookupEnv(serverAddressKey, defaultServerAddress)
+	_, _, err := net.SplitHostPort(v)
+	if err != nil {
+		panic(fmt.Errorf("[error] Failed to lookup server address: %s", err))
+	}
+	return v
+}
+
 func lookupEnv(key, defaultValue string) string {
 	v, ok := os.LookupEnv(key)
 	if ok {
