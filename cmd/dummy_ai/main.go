7f1e0421779b1075faf894331a086199e84244aa
Tarcisio Oliveira <t@rcis.io>
2025-08-08 12:26:31 +0000

    update

--- cmd/dummy_ai/main.go
+++ cmd/dummy_ai/main.go
@@ -1,9 +1,12 @@
+// Package main is the entry point for the dummy_ai application.
 package main
 
 import (
 	"dummy_ai/server"
 )
 
+// main is the entry point for the dummy_ai application.
+// It starts the web server by calling the server.Start() function.
 func main() {
 	server.Start()
 }
