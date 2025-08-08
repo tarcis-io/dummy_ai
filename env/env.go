26721e9e7352f979b55f6128b4032c6ea98dbb33
Tarcisio Oliveira <t@rcis.io>
2025-08-08 13:12:25 +0000

    update

--- env/env.go
+++ env/env.go
@@ -1,3 +1,4 @@
+// Package env provides access to environment variables.
 package env
 
 import (
@@ -5,6 +6,7 @@ import (
 )
 
 type (
+	// Config represents the configuration settings for the application.
 	Config struct {
 		languageCode  string
 		serverAddress string
@@ -12,22 +14,29 @@ type (
 	}
 )
 
+// LanguageCode returns the language code for the application.
 func (c *Config) LanguageCode() string {
 	return c.languageCode
 }
 
+// ServerAddress returns the server address for the application.
 func (c *Config) ServerAddress() string {
 	return c.serverAddress
 }
 
+// ThemeCode returns the theme code for the application.
 func (c *Config) ThemeCode() string {
 	return c.themeCode
 }
 
 var (
+	// currentConfig holds the current configuration settings for the application.
 	currentConfig *Config
 )
 
+// init runs on startup.
+// It configures the package's initial state.
+// 1. The current configuration.
 func init() {
 	currentConfig = &Config{
 		languageCode:  LookupEnv("LANGUAGE_CODE", "en"),
@@ -36,22 +45,28 @@ func init() {
 	}
 }
 
+// CurrentConfig returns the current configuration settings for the application.
 func CurrentConfig() *Config {
 	return currentConfig
 }
 
+// LanguageCode returns the language code for the application.
 func LanguageCode() string {
 	return currentConfig.languageCode
 }
 
+// ServerAddress returns the server address for the application.
 func ServerAddress() string {
 	return currentConfig.serverAddress
 }
 
+// ThemeCode returns the theme code for the application.
 func ThemeCode() string {
 	return currentConfig.themeCode
 }
 
+// LookupEnv retrieves the value of the environment variable named by the key
+// or the default value if the variable is not present.
 func LookupEnv(key, defaultValue string) string {
 	v, ok := os.LookupEnv(key)
 	if ok {
