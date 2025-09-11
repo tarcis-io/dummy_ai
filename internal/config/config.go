f30c827fb8e6c85b69f141ce510a9b661cd81a08
Tarcisio Oliveira <t@rcis.io>
2025-09-11 03:12:05 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,4 @@
+// Package config provides configurations for the application.
 package config
 
 import (
@@ -7,16 +8,20 @@ import (
 )
 
 type (
+	// Config holds the configurations for the application.
 	Config struct {
 		ServerAddress string
 	}
 )
 
 const (
+	// Server configurations.
 	serverAddressEnvKey     = "SERVER_ADDRESS"
 	serverAddressEnvDefault = "0.0.0.0:8080"
 )
 
+// New creates and returns a new Config instance by resolving the configurations for the application.
+// It returns an error if any of the configurations cannot be resolved.
 func New() (*Config, error) {
 	serverAddress, err := resolveServerAddress()
 	if err != nil {
@@ -28,6 +33,8 @@ func New() (*Config, error) {
 	return config, nil
 }
 
+// resolveServerAddress resolves the server address configuration for the application.
+// It returns an error if the configuration cannot be resolved.
 func resolveServerAddress() (string, error) {
 	serverAddress := getEnv(serverAddressEnvKey, serverAddressEnvDefault)
 	if _, _, err := net.SplitHostPort(serverAddress); err != nil {
@@ -36,6 +43,8 @@ func resolveServerAddress() (string, error) {
 	return serverAddress, nil
 }
 
+// getEnv retrieves the value of the environment variable with the given key.
+// It returns the default value if the environment variable is not set.
 func getEnv(envKey, envDefault string) string {
 	if envValue, found := os.LookupEnv(envKey); found {
 		return envValue
