e1a34fbafdaadbbe4a6e803c8de9bc880086a00a
Tarcisio Oliveira <t@rcis.io>
2025-09-11 03:34:25 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,4 +5,14 @@ import (
 )
 
 func TestNew(t *testing.T) {
+	testCases := []struct {
+		name              string
+		envValues         map[string]string
+		wantServerAddress string
+		wantError         bool
+	}{}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+		})
+	}
 }
