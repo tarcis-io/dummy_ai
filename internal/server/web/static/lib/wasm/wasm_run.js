c33a00979f495abb830b94932de00f18e9abc60b
Tarcisio Oliveira <t@rcis.io>
2025-09-13 02:58:59 +0000

    update

--- internal/server/web/static/lib/wasm/wasm_run.js
+++ internal/server/web/static/lib/wasm/wasm_run.js
@@ -1,5 +1,15 @@
+/*
+ * "use strict" enforces stricter parsing in JavaScript.
+ */
 "use strict";
 
+/*
+ * wasmRun fetches, instaniates and runs a WebAssembly module
+ * compiled with Go.
+ *
+ * @param {string} wasmPath - The path to the Go WebAssembly module.
+ * @returns {void}
+ */
 const wasmRun = wasmPath => {
 	const go = new Go();
 	WebAssembly.instantiateStreaming(fetch(wasmPath), go.importObject).then(webAssemblyInstantiatedSource => {
