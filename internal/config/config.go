f4b9288f9c5b8d0aab43a17f236b1267b552bcd1
Tarcisio Oliveira <t@rcis.io>
2025-09-21 02:53:50 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,11 @@ type (
 	}
 )
 
+const (
+	serverAddressEnvKey     = "SERVER_ADDRESS"
+	serverAddressEnvDefault = "0.0.0.0:8080"
+)
+
 func getEnv(envKey, envDefault string) string {
 	if envValue, found := os.LookupEnv(envKey); found {
 		return envValue
