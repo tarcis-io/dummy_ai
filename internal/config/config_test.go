15b02118f020dc3947e08274fcf9e7d3e96861bc
Tarcisio Oliveira <t@rcis.io>
2025-09-21 03:48:08 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -13,6 +13,19 @@ func TestNew(t *testing.T) {
 	}{}
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
+			for envKey, envValue := range testCase.envValues {
+				t.Setenv(envKey, envValue)
+			}
+			config, err := New()
+			if testCase.wantError {
+				if err == nil {
+					t.Fatal("New() error got=nil want!=nil")
+				}
+				if config != nil {
+					t.Fatal("New() *Config got!=nil want=nil")
+				}
+				return
+			}
 		})
 	}
 }
