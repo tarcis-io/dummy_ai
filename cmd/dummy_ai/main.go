25697798be175f2a07d6a1d9fde6e9397f4bdf0c
Tarcisio Oliveira <t@rcis.io>
2025-09-22 03:32:44 +0000

    update

--- cmd/dummy_ai/main.go
+++ cmd/dummy_ai/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
 	"dummy_ai/internal/config"
@@ -10,13 +11,16 @@ import (
 func main() {
 	config, err := config.New()
 	if err != nil {
+		slog.Error("Failed to load configurations", "error", err)
 		os.Exit(1)
 	}
 	server, err := server.New()
 	if err != nil {
+		slog.Error("Failed to create server", "error", err)
 		os.Exit(1)
 	}
 	if err := server.Start(config.ServerAddress); err != nil {
+		slog.Error("Failed to start server", "error", err)
 		os.Exit(1)
 	}
 }
