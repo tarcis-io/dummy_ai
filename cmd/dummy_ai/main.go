4c2025b67511d5f000667c25ebd1476644c5497a
Tarcisio Oliveira <t@rcis.io>
2025-09-22 03:10:06 +0000

    update

--- cmd/dummy_ai/main.go
+++ cmd/dummy_ai/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"os"
+
+	"dummy_ai/internal/config"
+	"dummy_ai/internal/server"
+)
+
 func main() {
+	config, err := config.New()
+	if err != nil {
+		os.Exit(1)
+	}
+	server, err := server.New()
+	if err != nil {
+		os.Exit(1)
+	}
+	if err := server.Start(config.ServerAddress); err != nil {
+		os.Exit(1)
+	}
 }
