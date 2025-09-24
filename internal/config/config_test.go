de172ffc2763760a3fb66dabedd6a0bd6b5054dd
Tarcisio Oliveira <t@rcis.io>
2025-09-24 03:56:44 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,6 +7,8 @@ import (
 )
 
 // TestNew verifies if the New function correctly creates a new Config instance.
+// It covers default configurations, custom valid configurations
+// and error handling for invalid configurations.
 func TestNew(t *testing.T) {
 	mustParseDuration := func(duration string) time.Duration {
 		timeDuration, err := time.ParseDuration(duration)
