63b8c4adc45f0c63a4304d20a78e949951d769b4
Tarcisio Oliveira <t@rcis.io>
2025-09-24 16:41:06 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+type (
+	// testCase
+	testCase struct {
+		name       string
+		envValues  map[string]string
+		wantConfig *Config
+		wantError  bool
+	}
+)
+
 // TestNew verifies if the New function correctly creates a new Config instance.
 // It covers default configurations, custom valid configurations
 // and error handling for invalid configurations.
