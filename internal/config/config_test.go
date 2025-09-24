46ac1be09056cb0fe0596cb17a183728b77c3d9b
Tarcisio Oliveira <t@rcis.io>
2025-09-24 01:54:01 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,40 +7,52 @@ import (
 // TestNew verifies if the New function correctly creates a new Config instance.
 func TestNew(t *testing.T) {
 	testCases := []struct {
-		name              string
-		envValues         map[string]string
-		wantServerAddress string
-		wantError         bool
+		name       string
+		envValues  map[string]string
+		wantConfig *Config
+		wantError  bool
 	}{
 		{
-			name:              "should create a new Config instance with default values",
-			envValues:         map[string]string{},
-			wantServerAddress: serverAddressEnvDefault,
-			wantError:         false,
+			name:      "should create a new Config instance with default values",
+			envValues: map[string]string{},
+			wantConfig: &Config{
+				ServerAddress: serverAddressEnvDefault,
+			},
+			wantError: false,
 		},
 		{
-			name:              "should create a new Config instance with custom server address: 127.0.0.1:8081",
-			envValues:         map[string]string{serverAddressEnvKey: "127.0.0.1:8081"},
-			wantServerAddress: "127.0.0.1:8081",
-			wantError:         false,
+			name: "should create a new Config instance with custom server address: 127.0.0.1:8081",
+			envValues: map[string]string{
+				serverAddressEnvKey: "127.0.0.1:8081",
+			},
+			wantConfig: &Config{
+				ServerAddress: "127.0.0.1:8081",
+			},
+			wantError: false,
 		},
 		{
-			name:              "should create a new Config instance with custom server address: localhost:3000",
-			envValues:         map[string]string{serverAddressEnvKey: "localhost:3000"},
-			wantServerAddress: "localhost:3000",
-			wantError:         false,
+			name: "should create a new Config instance with custom server address: localhost:3000",
+			envValues: map[string]string{
+				serverAddressEnvKey: "localhost:3000",
+			},
+			wantConfig: &Config{
+				ServerAddress: "localhost:3000",
+			},
+			wantError: false,
 		},
 		{
-			name:              "should return an error if the server address cannot be resolved: empty",
-			envValues:         map[string]string{serverAddressEnvKey: ""},
-			wantServerAddress: "",
-			wantError:         true,
+			name: "should return an error if the server address cannot be resolved: empty",
+			envValues: map[string]string{
+				serverAddressEnvKey: "",
+			},
+			wantError: true,
 		},
 		{
-			name:              "should return an error if the server address cannot be resolved: localhost:999999",
-			envValues:         map[string]string{serverAddressEnvKey: "localhost:999999"},
-			wantServerAddress: "",
-			wantError:         true,
+			name: "should return an error if the server address cannot be resolved: localhost:999999",
+			envValues: map[string]string{
+				serverAddressEnvKey: "localhost:999999",
+			},
+			wantError: true,
 		},
 	}
 	for _, testCase := range testCases {
@@ -64,8 +76,8 @@ func TestNew(t *testing.T) {
 			if config == nil {
 				t.Fatal("New() *Config got=nil want!=nil")
 			}
-			if config.ServerAddress != testCase.wantServerAddress {
-				t.Errorf("New() *Config.ServerAddress got=%q want=%q", config.ServerAddress, testCase.wantServerAddress)
+			if config.ServerAddress != testCase.wantConfig.ServerAddress {
+				t.Errorf("New() *Config.ServerAddress got=%q want=%q", config.ServerAddress, testCase.wantConfig.ServerAddress)
 			}
 		})
 	}
