af52854a697282585e28912ebabf30c6431caccf
Tarcisio Oliveira <t@rcis.io>
2025-09-25 03:38:17 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,19 @@
 package server
+
+import (
+	"dummy_ai/internal/config"
+)
+
+type (
+	Server struct {
+	}
+)
+
+func New(config *config.Config) (*Server, error) {
+	server := &Server{}
+	return server, nil
+}
+
+func (server *Server) Run() error {
+	return nil
+}
