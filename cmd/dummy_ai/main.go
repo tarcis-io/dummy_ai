898f82fc465db3682dad33366bc4e0efa3ec90a2
Tarcisio Oliveira <t@rcis.io>
2025-09-25 14:02:28 +0000

    update

--- cmd/dummy_ai/main.go
+++ cmd/dummy_ai/main.go
@@ -1,5 +1,16 @@
 // Package main is the entry point for the dummy_ai application.
 package main
 
+import (
+	"os"
+)
+
 func main() {
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	return nil
 }
