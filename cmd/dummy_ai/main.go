1b8fe0d17e3d4a0b5908dcf253b8c80ff07d786b
Tarcisio Oliveira <t@rcis.io>
2025-09-25 14:12:30 +0000

    update

--- cmd/dummy_ai/main.go
+++ cmd/dummy_ai/main.go
@@ -2,13 +2,18 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 )
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+	slog.Info("starting application")
 	if err := run(); err != nil {
+		slog.Error("application failed", "error", err)
 		os.Exit(1)
 	}
+	slog.Info("application exited")
 }
 
 func run() error {
