47aed258be3715d7aa3a1e7f8853cc9cc2e0af62
Tarcisio Oliveira <t@rcis.io>
2025-09-26 22:07:38 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"net/http"
 	"time"
 
 	"dummy_ai/internal/config"
@@ -9,14 +10,17 @@ import (
 type (
 	Server struct {
 		address         string
+		router          *http.ServeMux
 		shutdownTimeout time.Duration
 	}
 )
 
 func New(config *config.Config) (*Server, error) {
+	router := http.NewServeMux()
 	server := &Server{
 		address:         config.ServerAddress,
 		shutdownTimeout: config.ServerShutdownTimeout,
+		router:          router,
 	}
 	return server, nil
 }
